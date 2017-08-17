7ae36e2f0815ae42bad745ea63b36b414a44971a
Vyacheslav Yudanov <judanov.v@gmail.com>
2017-08-18 00:25:07 +0300

    Added initial module

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,13 @@
+using Nancy;
+
+public class HomeModule : NancyModule
+{
+	public HomeModule()
+	{
+		Get("/", args => "Hello World");
+ 
+		Get("/SayHello", args => $"Hello {this.Request.Query["name"]}");
+ 
+		Get("/SayHello2/{name}", args => $"Hello {args.name}");
+	}
+}
