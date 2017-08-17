0bfd029901e9477bd3257835fe44cae1abc14171
Vyacheslav Yudanov <judanov.v@gmail.com>
2017-08-18 00:25:28 +0300

    Removed redundant routes

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -5,9 +5,5 @@ public class HomeModule : NancyModule
 	public HomeModule()
 	{
 		Get("/", args => "Hello World");
- 
-		Get("/SayHello", args => $"Hello {this.Request.Query["name"]}");
- 
-		Get("/SayHello2/{name}", args => $"Hello {args.name}");
 	}
 }
